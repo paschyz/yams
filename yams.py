4f670094ddba371ebb261cb1077dbde4cc921fe1
paschyz <paschy2002@gmail.com>
2023-12-21 08:53:30 +0100

    wrote basic tests for functions detecting figures

--- yams.py
+++ yams.py
@@ -1,8 +1,9 @@
 class Yams:
-    figures= {
+    figures = {
         "brelan":[28,True],
         "carre":[35,True],
         "full":[30,True],
         "grande_suite":[40,True],
         "yams":[50,True],
     }
+    
--- yams_test.py
+++ yams_test.py
@@ -27,12 +27,39 @@ class TestYams(unittest.TestCase):
         yams=Yams()
         result = yams.calculate_points([6,6,6,6,6])
         self.assertEqual(result,50)
-    
+        
     def test_calculate_points_for_chance(self):
         yams=Yams()
         result = yams.calculate_points([1,3,4,5,6])
         sum = 1+3+4+5+6 #=19
-        self.assertEqual(result,sum) 
+        self.assertEqual(result,sum)     
         
+        
+    def test_is_brelan(self):
+        yams=Yams()
+        result = yams.is_brelan([6,6,6,1,2])
+        self.assertEqual(result,True) 
+        
+    def test_is_carre(self):
+        yams=Yams()
+        result = yams.is_carre([6,6,6,6,1])
+        self.assertEqual(result,True) 
+        
+    def test_is_full(self):
+        yams=Yams()
+        result = yams.is_full([6,6,6,2,2])
+        self.assertEqual(result,True) 
+        
+    def test_is_grande_suite(self):
+        yams=Yams()
+        result = yams.is_grande_suite([1,2,3,4,5])
+        self.assertEqual(result,True) 
+        
+    def test_is_yams(self):
+        yams=Yams()
+        result = yams.is_yams([6,6,6,6,6])
+        self.assertEqual(result,True) 
+    
+    
 if __name__ == '__main__':
     unittest.main()
