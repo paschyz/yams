109d56e5340fa19c9afdbeb7c5d8a2f80e56783d
paschyz <paschy2002@gmail.com>
2023-12-21 09:15:35 +0100

    added tests for false case

--- yams_test.py
+++ yams_test.py
@@ -59,7 +59,31 @@ class TestYams(unittest.TestCase):
         yams=Yams()
         result = yams.is_yams([6,6,6,6,6])
         self.assertEqual(result,True) 
-    
-    
+        
+    def test_is_brelan_false(self):
+        yams = Yams()
+        result = yams.is_brelan([1, 2, 3, 4, 5])
+        self.assertEqual(result, False)  
+
+    def test_is_carre_false(self):
+        yams = Yams()
+        result = yams.is_carre([1, 2, 3, 4, 5])
+        self.assertEqual(result, False) 
+
+    def test_is_full_false(self):
+        yams = Yams()
+        result = yams.is_full([1, 2, 3, 4, 5])
+        self.assertEqual(result, False) 
+
+    def test_is_grande_suite_false(self):
+        yams = Yams()
+        result = yams.is_grande_suite([1, 2, 3, 4, 6])
+        self.assertEqual(result, False)  
+
+    def test_is_yams_false(self):
+        yams = Yams()
+        result = yams.is_yams([1, 2, 3, 4, 5])
+        self.assertEqual(result, False) 
+        
 if __name__ == '__main__':
     unittest.main()
