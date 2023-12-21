739c210fb4a7e4ded056898ac6554e97644e7b30
paschyz <paschy2002@gmail.com>
2023-12-21 09:30:00 +0100

    added special case test

--- yams.py
+++ yams.py
@@ -1,5 +1,6 @@
 class Yams:
-    figures = {
+    def __init__ (self):
+        self.figures = {
         "brelan":[28,True],
         "carre":[35,True],
         "full":[30,True],
@@ -8,19 +9,24 @@ class Yams:
     }
     
     def calculate_points(self, dice_results):
-        
-        if(self.is_yams(dice_results) and self.figures["yams"][1] == True):
+        if self.is_yams(dice_results) and self.figures["yams"][1]:
+            self.figures["yams"][1] = False
             return self.figures["yams"][0]
-        elif(self.is_carre(dice_results) and self.figures["carre"][1] == True):
+        elif self.is_carre(dice_results) and self.figures["carre"][1]:
+            self.figures["carre"][1] = False
             return self.figures["carre"][0]
-        elif(self.is_full(dice_results) and self.figures["full"][1] == True):
+        elif self.is_full(dice_results) and self.figures["full"][1]:
+            self.figures["full"][1] = False
             return self.figures["full"][0]
-        elif(self.is_brelan(dice_results) and self.figures["brelan"][1] == True):
+        elif self.is_brelan(dice_results) and self.figures["brelan"][1]:
+            self.figures["brelan"][1] = False
             return self.figures["brelan"][0]
-        elif(self.is_grande_suite(dice_results) and self.figures["grande_suite"][1] == True):
+        elif self.is_grande_suite(dice_results) and self.figures["grande_suite"][1]:
+            self.figures["grande_suite"][1] = False
             return self.figures["grande_suite"][0]
         else:
             return sum(dice_results)
+
         
         
 
--- yams_test.py
+++ yams_test.py
@@ -2,32 +2,40 @@ import unittest
 from yams import Yams
 
 class TestYams(unittest.TestCase):
+    figurePoints = {
+        "brelan": 28,
+        "carre": 35,
+        "full": 30,
+        "grande_suite": 40,
+        "yams": 50,
+    }
 
+    
     def test_calculate_points_for_brelan(self):
         yams=Yams()
         result = yams.calculate_points([6,6,6,1,2])
-        self.assertEqual(result,28)
+        self.assertEqual(result,self.figurePoints["brelan"])
         
     def test_calculate_points_for_carre(self):
         yams=Yams()
         result = yams.calculate_points([6,6,6,6,2])
-        self.assertEqual(result,35)
-        
+        self.assertEqual(result,self.figurePoints["carre"])
+                
     def test_calculate_points_for_full(self):
         yams=Yams()
         result = yams.calculate_points([6,6,6,2,2])
-        self.assertEqual(result,30)
-        
+        self.assertEqual(result,self.figurePoints["full"])
+                        
     def test_calculate_points_for_grande_suite(self):
         yams=Yams()
         result = yams.calculate_points([1,2,3,4,5])
-        self.assertEqual(result,40) 
-        
+        self.assertEqual(result,self.figurePoints["grande_suite"])
+                        
     def test_calculate_points_for_yams(self):
         yams=Yams()
         result = yams.calculate_points([6,6,6,6,6])
-        self.assertEqual(result,50)
-        
+        self.assertEqual(result,self.figurePoints["yams"])
+                        
     def test_calculate_points_for_chance(self):
         yams=Yams()
         result = yams.calculate_points([1,3,4,5,6])
@@ -84,6 +92,15 @@ class TestYams(unittest.TestCase):
         yams = Yams()
         result = yams.is_yams([1, 2, 3, 4, 5])
         self.assertEqual(result, False) 
+        
+    def test_calculate_points_for_multiple_yams(self):
+        yams = Yams()
+        result = yams.calculate_points([6, 6, 6, 6, 6])
+        self.assertEqual(result, self.figurePoints["yams"])
+
+        # Second throw also yams
+        result = yams.calculate_points([1, 1, 1, 1, 1])
+        self.assertEqual(result, self.figurePoints["carre"])
         
 if __name__ == '__main__':
     unittest.main()
