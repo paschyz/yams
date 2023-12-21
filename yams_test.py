a4aab11ebeb15a1071c3dec6cfb2b55c47587707
paschyz <paschy2002@gmail.com>
2023-12-21 08:34:21 +0100

    wrote basic tests for calculate_points function

--- yams_test.py
+++ yams_test.py
@@ -1,11 +1,38 @@
 import unittest
-
+from yams import Yams
 
 class TestYams(unittest.TestCase):
-    def test_one_plus_one(self):
-        one_plus_one=1+1
-        self.assertEqual(one_plus_one, 2)
 
-
+    def test_calculate_points_for_brelan(self):
+        yams=Yams()
+        result = yams.calculate_points([6,6,6,1,2])
+        self.assertEqual(result,28)
+        
+    def test_calculate_points_for_carre(self):
+        yams=Yams()
+        result = yams.calculate_points([6,6,6,6,2])
+        self.assertEqual(result,35)
+        
+    def test_calculate_points_for_full(self):
+        yams=Yams()
+        result = yams.calculate_points([6,6,6,2,2])
+        self.assertEqual(result,30)
+        
+    def test_calculate_points_for_grande_suite(self):
+        yams=Yams()
+        result = yams.calculate_points([1,2,3,4,5])
+        self.assertEqual(result,40) 
+        
+    def test_calculate_points_for_yams(self):
+        yams=Yams()
+        result = yams.calculate_points([6,6,6,6,6])
+        self.assertEqual(result,50)
+    
+    def test_calculate_points_for_chance(self):
+        yams=Yams()
+        result = yams.calculate_points([1,3,4,5,6])
+        sum = 1+3+4+5+6 #=19
+        self.assertEqual(result,sum) 
+        
 if __name__ == '__main__':
     unittest.main()
