4ae1e340d9a14da8ed2e367f3e66bdbd4c23bc38
paschyz <paschy2002@gmail.com>
2023-12-21 09:09:32 +0100

    added methods to verify figures and calculate points

--- yams.py
+++ yams.py
@@ -7,3 +7,42 @@ class Yams:
         "yams":[50,True],
     }
     
+    def calculate_points(self, dice_results):
+        
+        if(self.is_yams(dice_results) and self.figures["yams"][1] == True):
+            return self.figures["yams"][0]
+        elif(self.is_carre(dice_results) and self.figures["carre"][1] == True):
+            return self.figures["carre"][0]
+        elif(self.is_full(dice_results) and self.figures["full"][1] == True):
+            return self.figures["full"][0]
+        elif(self.is_brelan(dice_results) and self.figures["brelan"][1] == True):
+            return self.figures["brelan"][0]
+        elif(self.is_grande_suite(dice_results) and self.figures["grande_suite"][1] == True):
+            return self.figures["grande_suite"][0]
+        else:
+            return sum(dice_results)
+        
+        
+
+    
+    def is_brelan(self, dice_results):
+        return len(set(dice_results)) <= 3
+    
+    def is_carre(self, dice_results):
+        for value in set(dice_results):
+            if dice_results.count(value) >= 4:
+                return True
+        return False
+
+    def is_full(self, dice_results):
+        counts = [dice_results.count(value) for value in set(dice_results)]
+        return sorted(counts) == [2, 3]
+
+    def is_grande_suite(self, dice_results):
+        sorted_dice = sorted(dice_results)
+
+        return sorted_dice == [1, 2, 3, 4, 5] or sorted_dice == [2, 3, 4, 5, 6]
+
+
+    def is_yams(self, dice_results):
+        return len(set(dice_results)) == 1
